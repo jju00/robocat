8dfe1d345a99fa69cec1966f24016c1f13c4d34a
GHYoungKyun <dudrbsapdlf@naver.com>
2026-02-18 15:59:30 +0900

    extraction bug fixed

--- scripts/run_php_extractor.py
+++ scripts/run_php_extractor.py
@@ -1,8 +1,16 @@
 import subprocess
+import sys
+
+# Python 실행 시 전달된 인자들
+args = sys.argv[1:]  # 스크립트 이름 제외
+
+if not args:
+    print("Usage: python run_extractors.py <target_dir> [output_dir]")
+    sys.exit(1)
 
 scripts = [
-    ["php", "ast-extractor.php"],
-    ["php", "event_routes_extractor.php"]
+    ["php", "ast-extractor.php", *args],
+    ["php", "event_routes_extractor.php", *args]
 ]
 
 processes = [subprocess.Popen(cmd) for cmd in scripts]
@@ -11,3 +19,4 @@ for p in processes:
     p.wait()
 
 print("All scripts finished.")
+
